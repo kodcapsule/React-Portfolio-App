eac5a501bb32f4b9767fb378de678d2fcc4552f7
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-03 22:00:08 +0000

    commplated about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,39 +14,41 @@ const About = () => {
 
       <div className="container about__container">
         <div className="me_container">
-          <img src={Me} alt="Me" />
+          <img src={Me} alt="Me" className="me__img" />
         </div>
 
         <div className="about_container">
-          <article className="about_card">
-            <FaAward className="abpou__icon" />
-            <h5>Experience</h5>
-            <small>3 + years</small>
-          </article>
+          <div className="cards-container">
+            <article className="about_card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3 + years</small>
+            </article>
 
-          <article className="about_card">
-            <FiUsers className="abpou__icon" />
-            <h5>Clients</h5>
-            <small>200+ Clients</small>
-          </article>
+            <article className="about_card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Clients</small>
+            </article>
 
-          <article className="about_card">
-            <MdOutlineWork className="abpou__icon" />
-            <h5>Work</h5>
-            <small>80 + Projects</small>
-          </article>
+            <article className="about_card">
+              <MdOutlineWork className="about__icon" />
+              <h5>Work</h5>
+              <small>80 + Projects</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+            ex quisquam velit eligendi fuga, eum temporibus sapiente architecto
+            odio, dolorum quas accusantium exercitationem nihil a nam quod amet.
+            Harum, ipsam?
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Contact Me
+          </a>
         </div>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ex
-          quisquam velit eligendi fuga, eum temporibus sapiente architecto odio,
-          dolorum quas accusantium exercitationem nihil a nam quod amet. Harum,
-          ipsam?
-        </p>
       </div>
-      <a href="#contact" className="btn btn-primary">
-        Contact Me
-      </a>
     </section>
   );
 };
