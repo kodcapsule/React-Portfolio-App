f89bcd79ab1458fa939da7a17615bff52d041e7d
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-13 22:33:43 +0000

    added icons to tech stacks

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,8 +3,14 @@ import "./experience.css";
 import { FaCheckCircle } from "react-icons/fa";
 import { ImHtmlFive } from "react-icons/im";
 import { IoLogoCss3 } from "react-icons/io";
-import { TbBrandJavascript } from "react-icons/tb";
+import { DiJavascript1 } from "react-icons/di";
 import { SiDjango } from "react-icons/si";
+import { FaBootstrap } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { RiReactjsLine } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { SiPostgresql } from "react-icons/si";
 const Experience = () => {
   return (
     <section>
@@ -26,19 +32,19 @@ const Experience = () => {
               <h4>CSS</h4>
             </article>
             <article className="experince_details">
-              <TbBrandJavascript className="check_icon" />
+              <DiJavascript1 className="check_icon" />
               <h4>JAVASCRIPT</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle className="check_icon" />
+              <FaBootstrap className="check_icon" />
               <h4>BOOTSRAP</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle className="check_icon" />
+              <SiTailwindcss className="check_icon" />
               <h4>TAILWIND</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle className="check_icon" />
+              <RiReactjsLine className="check_icon" />
               <h4>REACT</h4>
             </article>
           </div>
@@ -48,7 +54,7 @@ const Experience = () => {
           <h3>BACKEND Development</h3>
           <div className="experinece__content">
             <article className="experince_details">
-              <FaCheckCircle className="check_icon" />
+              <FaNodeJs className="check_icon" />
               <h4>NodeJS</h4>
             </article>
             <article className="experince_details">
@@ -56,11 +62,11 @@ const Experience = () => {
               <h4>Django</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle className="check_icon" />
+              <GrMysql className="check_icon" />
               <h4>MYSQL</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle className="check_icon" />
+              <SiPostgresql className="check_icon" />
               <h4>POSTGRE</h4>
             </article>
             <article className="experince_details">
