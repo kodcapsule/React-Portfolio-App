51dd399e47fd807c3d5f3e19f7ccad7c73483b76
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-13 22:17:20 +0000

    styled experienced section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./experience.css";
 import { FaCheckCircle } from "react-icons/fa";
+import { ImHtmlFive } from "react-icons/im";
+import { IoLogoCss3 } from "react-icons/io";
+import { TbBrandJavascript } from "react-icons/tb";
+import { SiDjango } from "react-icons/si";
 const Experience = () => {
   return (
     <section>
@@ -12,27 +16,29 @@ const Experience = () => {
           <h3>FRONTEND Develoepment</h3>
           <div className="experinece__content">
             <article className="experince_details">
-              <FaCheckCircle />
+              {/* <FaCheckCircle className="check_icon" /> */}
+              <ImHtmlFive className="check_icon" />
               <h4>HTML</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle />
+              <IoLogoCss3 className="check_icon" />
+
               <h4>CSS</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle />
+              <TbBrandJavascript className="check_icon" />
               <h4>JAVASCRIPT</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle />
+              <FaCheckCircle className="check_icon" />
               <h4>BOOTSRAP</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle />
+              <FaCheckCircle className="check_icon" />
               <h4>TAILWIND</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle />
+              <FaCheckCircle className="check_icon" />
               <h4>REACT</h4>
             </article>
           </div>
@@ -42,23 +48,23 @@ const Experience = () => {
           <h3>BACKEND Development</h3>
           <div className="experinece__content">
             <article className="experince_details">
-              <FaCheckCircle />
+              <FaCheckCircle className="check_icon" />
               <h4>NodeJS</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle />
+              <SiDjango className="check_icon" />
               <h4>Django</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle />
+              <FaCheckCircle className="check_icon" />
               <h4>MYSQL</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle />
+              <FaCheckCircle className="check_icon" />
               <h4>POSTGRE</h4>
             </article>
             <article className="experince_details">
-              <FaCheckCircle />
+              <FaCheckCircle className="check_icon" />
               <h4>MANGO DB</h4>
             </article>
           </div>
