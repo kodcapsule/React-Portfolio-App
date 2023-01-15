d18fb4db50f2559e8782b4aef30c5e3b8021efe9
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-15 22:45:52 +0000

    added the portfolio ssection

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,12 @@
-import React from "react";
 import "./portfolio.css";
 
 const Portfolio = () => {
-  return <section></section>;
+  return (
+    <section>
+      <h5>My recent Projects</h5>
+      <h2>Projects</h2>
+    </section>
+  );
 };
 
 export default Portfolio;
