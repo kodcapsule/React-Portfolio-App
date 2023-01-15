a4cd4b199e30e2f4c5e867340188425ec1d83ae5
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-15 21:15:18 +0000

    added the ther services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,100 @@
-import React from "react";
 import "./services.css";
+import { GoCheck } from "react-icons/go";
 
 const Services = () => {
-  return <section></section>;
+  return (
+    <section>
+      <h5>What i offer</h5>
+      <h2>Services</h2>
+      <div className="container service__container">
+        <article className="services">
+          <h3>UI/UX</h3>
+          <ul className="Ssrvice_list">
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="services">
+          <h3>Web Development</h3>
+          <ul className="Ssrvice_list">
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="services">
+          <h3>Content Creation</h3>
+          <ul className="Ssrvice_list">
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li className="service">
+              <GoCheck />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
