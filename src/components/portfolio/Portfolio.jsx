870f36befcb94cab2ee5a71cb448fcdb36899add
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-19 22:09:52 +0000

    added the portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,6 +5,113 @@ const Portfolio = () => {
     <section>
       <h5>My recent Projects</h5>
       <h2>Projects</h2>
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="image_container">
+            <img src="" alt="" />
+          </div>
+          <h3>This is my project</h3>
+
+          <div className="">
+            <a
+              href="https://github.com/kodcapsule/React-Portfolio-App"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com/kodcapsule/React-Portfolio-App"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="image_container">
+            <img src="" alt="" />
+          </div>
+          <h3>This is my project</h3>
+
+          <div className="">
+            <a
+              href="https://github.com/kodcapsule/React-Portfolio-App"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com/kodcapsule/React-Portfolio-App"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="image_container">
+            <img src="" alt="" />
+          </div>
+          <h3>This is my project</h3>
+
+          <div className="">
+            <a
+              href="https://github.com/kodcapsule/React-Portfolio-App"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com/kodcapsule/React-Portfolio-App"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="image_container">
+            <img src="" alt="" />
+          </div>
+          <h3>This is my project</h3>
+
+          <div className="">
+            <a
+              href="https://github.com/kodcapsule/React-Portfolio-App"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com/kodcapsule/React-Portfolio-App"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="image_container">
+            <img src="" alt="" />
+          </div>
+          <h3>This is my project</h3>
+
+          <div className="">
+            <a
+              href="https://github.com/kodcapsule/React-Portfolio-App"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com/kodcapsule/React-Portfolio-App"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
     </section>
   );
 };
